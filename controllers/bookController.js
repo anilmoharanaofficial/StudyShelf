a40624410c042f646e0cd4423168728f1197ea5f
Anil Moharana <anil.k.moharana@gmail.com>
2024-05-31 20:49:06 +0530

    Added CatchAsync Util

---
+++ controllers/bookController.js
@@ -0,0 +1,15 @@
+import catchAsync from "../utils/catchAsync.js";
+
+// PUBLISH BOOK
+const publish = catchAsync(async (req, res, next) => {});
+
+// VIEW
+const view = catchAsync(async (req, res, next) => {});
+
+// UPDATE
+const update = catchAsync(async (req, res, next) => {});
+
+// DELETE
+const deleteBook = catchAsync(async (req, res, next) => {});
+
+export { publish, view, update, deleteBook };
